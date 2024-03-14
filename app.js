392392d60fe9f5ac91d28bec140abdd585804e39
sheikhabdullahahmed <sheikhabdullahahmed>
2024-03-15 02:06:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// let minutes = 0;
+// let second  = 0;
+// let miilsec = 0;
+// let getmin = document.getElementById ("min")
+// let getsec = document.getElementById ("sec")
+// let getmsec = document.getElementById ("msec")
+// let interval;
+
+// function start () {
+//     interval = setInterval (function () {
+//         miilsec++
+//         getmsec.innerHTML = miilsec
+//         if (miilsec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             miilsec= 0
+//         }
+//         else if (second >=10) {
+//             minutes++
+//             getmsec.innerHTML = minutes
+//             second = 0
+//         }
+//         document.getElementById("btn").disabled = true
+//     } , 10)
+
+// }
+
+
+// function stop () {
+//     clearInterval (interval)
+//     document.getElementById("btn").disabled = false
+// }
+
+// function reset () {
+//     minutes = 0;
+//     second = 0;
+//     miilsec = 0;
+//     getmin.innerHTML = minutes;
+//     getsec.innerHTML = second;      
+//     getmsec.innerHTML = miilsec;
+// }
+ 
